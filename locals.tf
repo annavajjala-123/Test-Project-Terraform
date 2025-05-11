7c4372bb626172754b71b8920b3c5c56632131f4
Shravankumar Annavajjala <annavajjala321@gmail.com>
2025-05-11 19:40:07 +0530

    locals.tf

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  resource_group_name    = "${var.project_name}-${var.environment}-rg"
+  sql_server_name        = "${var.project_name}-${var.environment}-sql"
+  storage_account_name   = lower(replace("${var.project_name}${var.environment}stor", "-", ""))
+  web_app_name           = "${var.project_name}-${var.environment}-webapp"
+  app_service_plan_name  = "${var.project_name}-${var.environment}-plan"
+
+  common_tags = {
+    project     = var.project_name
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
