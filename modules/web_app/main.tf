db5851abd2f84c6de061213dad8607722f9da648
Shravankumar Annavajjala <annavajjala321@gmail.com>
2025-05-14 00:00:17 +0530

    Update main.tf

--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -1,25 +1,70 @@
-resource "azurerm_service_plan" "testingappserviceplan" {
-  name                = var.app_service_plan_name
+resource "azurerm_app_service_plan" "testasp" {
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "PremiumV2"
+    size = "P1v2"
   }
 
   tags = var.tags
 }
 
-resource "azurerm_app_service" "testingappservice" {
-  name                = var.name
+resource "azurerm_linux_web_app" "testwebapp" {
+  name                = "testwebapp"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.testingappserviceplan.id
+  service_plan_id     = azurerm_app_service_plan.testasp.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
+    always_on        = true
+    ftps_state       = "Disabled"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
+
+
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "link" {
+  name                  = "dns-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = var.vnet_id
+  registration_enabled  = false
+}
+
+resource "azurerm_private_endpoint" "pe" {
+  name                = "private"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "test-psc"
+    private_connection_resource_id = azurerm_linux_web_app.testwebapp.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns.id]
   }
 
   tags = var.tags
