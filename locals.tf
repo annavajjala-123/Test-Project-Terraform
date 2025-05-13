9fcb8e8c332ed8cf459825f3c284207eec4beeb8
Shravankumar Annavajjala <annavajjala321@gmail.com>
2025-05-13 23:56:40 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
   resource_group_name    = "${var.project_name}-${var.environment}-rg"
   sql_server_name        = "${var.project_name}-${var.environment}-sql"
+  vnet_name              = "${var.project_name}-${var.environment}-vnet"
   storage_account_name   = lower(replace("${var.project_name}${var.environment}stor", "-", ""))
   web_app_name           = "${var.project_name}-${var.environment}-webapp"
   app_service_plan_name  = "${var.project_name}-${var.environment}-plan"
